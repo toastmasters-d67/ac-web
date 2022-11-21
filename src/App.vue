18ba00cc52e7ba1162f838a7f355a6d5ba200f11
Don <dontwn@gmail.com>
2022-11-21 19:52:46 +0800

    Set font to Montserrat

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,19 @@
     url("./assets/fonts/Montserrat/Montserrat-Regular.woff2") format("woff2"),
     url("./assets/fonts/Montserrat/Montserrat-Regular.woff") format("woff");
 }
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
 
 <script>
