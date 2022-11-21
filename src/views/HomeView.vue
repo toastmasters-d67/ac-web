cdbf7276f186184f4f4fadf3420d0ccf5b5c1d11
Don <dontwn@gmail.com>
2022-11-21 20:42:17 +0800

    Update HomeView page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,14 @@
 <template>
-  <div class="home">
-    <Slogan v-once />
-    <Conference v-once />
-    <Speakers v-once />
-    <Souvenirs v-once />
-    <Ticket v-once />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-import Slogan from "@/components/home/Slogan.vue";
-import Conference from "@/components/home/Conference.vue";
-import Speakers from "@/components/home/Speakers.vue";
-import Souvenirs from "@/components/home/Souvenirs.vue";
-import Ticket from "@/components/home/Ticket.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
   components: {
-    Slogan,
-    Conference,
-    Speakers,
-    Souvenirs,
-    Ticket,
+    // HelloWorld,
   },
 };
 </script>
