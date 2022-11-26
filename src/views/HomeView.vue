10d1287c9cbd54d7254a71ade9e0f0dbff72f917
Don <dontwn@gmail.com>
2022-11-26 15:50:37 +0800

    Add Conference component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Conference v-once />
     <Speakers v-once />
     <Souvenirs v-once />
     <Ticket v-once />
@@ -7,6 +8,7 @@
 </template>
 
 <script>
+import Conference from "@/components/home/Conference.vue";
 import Speakers from "@/components/home/Speakers.vue";
 import Souvenirs from "@/components/home/Souvenirs.vue";
 import Ticket from "@/components/home/Ticket.vue";
@@ -14,6 +16,7 @@ import Ticket from "@/components/home/Ticket.vue";
 export default {
   name: "HomeView",
   components: {
+    Conference,
     Speakers,
     Souvenirs,
     Ticket,
