80ccd6a5f443c2e928a0049755ba03b057b962dd
Don <dontwn@gmail.com>
2022-12-13 10:17:47 +0800

    Add SpeakerTemplateView page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const routes = [
   {
@@ -11,6 +11,11 @@ const routes = [
     path: "/speakers",
     name: "speakers",
     component: () => import("@/views/SpeakersView.vue"),
+  },
+  {
+    path: "/speakers/:name",
+    name: "speaker",
+    component: () => import("@/views/SpeakerTemplateView.vue"),
   },
 ];
 
