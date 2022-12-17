c6444711d1a4fe69ab20cc57d428cbcd9e549b5c
Don <dontwn@gmail.com>
2022-12-17 15:41:31 +0800

    Update HomeView page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,31 @@
 <template>
   <article id="home">
     <Slogan v-once />
-    <Podcast v-once />
     <Speakers v-once />
     <Program v-once />
     <Souvenirs v-once />
     <Ticket v-once />
+    <Podcast v-once />
   </article>
 </template>
 
 <script>
 import Slogan from "@/components/home/Slogan.vue";
-import Podcast from "@/components/home/Podcast.vue";
 import Speakers from "@/components/home/Speakers.vue";
 import Program from "@/components/home/Program.vue";
 import Souvenirs from "@/components/home/Souvenirs.vue";
 import Ticket from "@/components/home/Ticket.vue";
+import Podcast from "@/components/home/Podcast.vue";
 
 export default {
   name: "HomeView",
   components: {
     Slogan,
-    Podcast,
     Speakers,
     Program,
     Souvenirs,
     Ticket,
+    Podcast,
   },
 };
 </script>
