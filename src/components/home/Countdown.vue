6f981b4019941c5ce1d6c5a9234669332483092c
Don <dontwn@gmail.com>
2023-01-06 15:56:14 +0800

    Update Countdown component

--- src/components/home/Countdown.vue
+++ src/components/home/Countdown.vue
@@ -2,42 +2,24 @@
   <section class="countdown-container">
     <div class="countdown-item">
       <span class="countdown-unit">Days</span>
-      <div class="countdown-circle">
-        <span class="countdown-number">{{ days }}</span>
-      </div>
+      <Timer :value="days" :full="120" />
     </div>
     <div class="countdown-item">
       <span class="countdown-unit">Hours</span>
-      <div class="countdown-circle">
-        <span class="countdown-number">{{ hours }}</span>
-      </div>
+      <Timer :value="hours" :full="24" />
     </div>
     <div class="countdown-item">
       <span class="countdown-unit">Minutes</span>
-      <div class="countdown-circle">
-        <span class="countdown-number">{{ minutes }}</span>
-      </div>
+      <Timer :value="minutes" :full="60" />
     </div>
     <div class="countdown-item">
       <span class="countdown-unit">Seconds</span>
-      <div class="countdown-circle">
-        <span class="countdown-number">{{ seconds }}</span>
-      </div>
+      <Timer :value="seconds" :full="60" />
     </div>
   </section>
 </template>
 
 <style scoped lang="scss">
-@font-face {
-  font-family: "Archivo";
-  font-weight: 400;
-  font-style: normal;
-  font-display: auto;
-  unicode-range: U+000-5FF;
-  src: local("Hack"),
-    url("@/assets/fonts/Archivo/Archivo-Regular.woff2") format("woff2"),
-    url("@/assets/fonts/Archivo/Archivo-Regular.woff") format("woff");
-}
 .countdown-container {
   width: 100%;
   background: #e5e5e5;
@@ -55,81 +37,54 @@
   .countdown-item {
     width: 200px;
     height: 273px;
+    display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     margin-top: 50px;
     margin-bottom: 50px;
     .countdown-unit {
-      color: white;
+      color: gray;
       font-size: 32px;
       font-weight: 500;
       line-height: 60px;
       text-align: center;
       text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      margin-bottom: 15px;
     }
-    .countdown-circle {
-      width: 200px;
-      height: 200px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      border-radius: 50%;
-      border: 4px solid #bc92b7;
-      border-top-color: white;
-      border-left-color: white;
-      border-bottom-color: white;
-      // animation: spin 1s ease-in-out infinite;
-      // animation: spin 60s linear infinite;
-      margin: 0 auto;
-      .countdown-number {
-        font-family: "Archivo";
-        font-size: 96px;
-        font-weight: 400;
-        line-height: 80px;
-        text-align: censter;
-        letter-spacing: 0.05em;
-        background: linear-gradient(38.59deg, #de809c 11.59%, #675de2 77.61%),
-          white;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        background-clip: text;
-        // text-shadow: 0px 6px 4px #c4c4c4;
-      }
-    }
-  }
-}
-@keyframes spin {
-  to {
-    transform: rotate(360deg);
   }
 }
 </style>
 
 <script>
 import { ref } from "vue";
+import Timer from "@/components/home/Timer.vue";
 
 export default {
   name: "Countdown",
+  components: {
+    Timer,
+  },
   data() {
-    const days = ref("98");
-    const hours = ref("06");
-    const minutes = ref("27");
-    const seconds = ref("40");
+    const [d, h, m, s] = this.getTime();
+    const days = ref(d);
+    const hours = ref(h);
+    const minutes = ref(m);
+    const seconds = ref(s);
+    const polling = ref(null);
     return {
       days,
       hours,
       minutes,
       seconds,
-      polling: null,
+      polling,
     };
   },
   created() {
     this.pollData();
   },
   methods: {
-    countdown() {
+    getTime() {
       const now = new Date();
       const date = new Date("apr 22, 2023 00:00:00");
       const remaining = date - now;
@@ -137,19 +92,14 @@ export default {
       const hours = Math.floor(remaining / (1000 * 60 * 60)) % 24;
       const minutes = Math.floor(remaining / (1000 * 60)) % 60;
       const seconds = Math.floor(remaining / 1000) % 60;
-      this.days = this.neat(days);
-      this.hours = this.neat(hours);
-      this.minutes = this.neat(minutes);
-      this.seconds = this.neat(seconds);
+      return [days, hours, minutes, seconds];
     },
-    neat(number) {
-      if (number < 0) {
-        return "";
-      }
-      if (number < 10) {
-        return `0${number.toString()}`;
-      }
-      return number.toString();
+    countdown() {
+      const [days, hours, minutes, seconds] = this.getTime();
+      this.days = days;
+      this.hours = hours;
+      this.minutes = minutes;
+      this.seconds = seconds;
     },
     pollData() {
       this.polling = window.setInterval(() => {
