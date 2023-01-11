b3eac28884e6978f6854ce008bfcbbfd8e894727
Aaron Lin <aaronlin1218@gmail.com>
2023-01-12 01:38:08 +0800

    Update Speakers to V4 design (desktop & mobile)
    (cherry picked from commit b5298115ea52a5114f099984eb5ed2fc1fff5d9b)

--- src/components/home/Speakers.vue
+++ src/components/home/Speakers.vue
@@ -36,18 +36,19 @@
   }
 }
 .speakers {
-  width: 1200px;
+  max-width: 1200px;
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
   border-color: transparent;
   margin: 0 auto;
+  padding-left: 70px;
+  margin-bottom: 32px;
   a {
     color: black;
     text-decoration: none;
     border-color: transparent;
   }
-  margin-bottom: 60px;
   .speaker {
     display: flex;
     flex-direction: column;
@@ -55,10 +56,10 @@
     align-items: center;
     border-color: transparent;
     margin-right: 70px;
-    margin-bottom: 60px;
+    margin-bottom: 50px;
     .speaker-image {
-      width: 300px;
-      height: 300px;
+      width: 250px;
+      height: 250px;
       border-radius: 20px;
       margin-bottom: 24px;
     }
@@ -71,7 +72,7 @@
 }
 .button-view-all {
   width: 170px;
-  background: linear-gradient(267.67deg, #ddc8bf 5.3%, #a7c4d4 95.87%);
+  background: #ffdb82;
   display: flex;
   flex-flow: row nowrap;
   justify-content: center;
@@ -81,16 +82,59 @@
   text-decoration: none;
   padding: 12px 24px;
   margin: 0 auto;
-  margin-bottom: 150px;
   .button-text {
-    color: white;
+    color: #6f6f6e;
     font-size: 22px;
     font-weight: 600;
     line-height: 27px;
     margin-right: 8px;
   }
   .button-arrow-right {
-    color: white;
+    color: #6f6f6e;
+  }
+}
+
+/* Mobile Design */
+@media screen and (max-width: 768px) {
+  .speakers-container {
+    padding-top: 50px;
+    padding-bottom: 50px;
+    .speakers-title {
+      font-size: 15px;
+      line-height: 18px;
+      margin-bottom: 29px;
+    }
+  }
+  .speakers {
+    padding-left: 10px;
+    margin-bottom: 0;
+    .speaker {
+      margin-right: 10px;
+      margin-bottom: 19px;
+      .speaker-image {
+        width: 100px;
+        height: 100px;
+        margin-bottom: 10px;
+      }
+      .speaker-name-text {
+        font-size: 12px;
+        line-height: 15px;
+      }
+    }
+  }
+  .button-view-all {
+    width: 91px;
+    border: 1px solid white;
+    padding: 8px 15px;
+    margin-bottom: 8px;
+    .button-text {
+      font-size: 10px;
+      line-height: 12px;
+      margin-right: 8px;
+    }
+    .button-arrow-right {
+      font-size: 9px;
+    }
   }
 }
 </style>
