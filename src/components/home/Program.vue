55fd8e13ca07a25e56ddfc7d3bbf80c48d6a93bd
Aaron Lin <aaronlin1218@gmail.com>
2023-01-17 02:02:01 +0800

    Update Program desktop design to V4

--- src/components/home/Program.vue
+++ src/components/home/Program.vue
@@ -1,124 +1,185 @@
 <template>
   <section id="program" class="program-container">
     <header class="program-title">Program</header>
+    <div class="program-day-switcher">
+      <div class="program-day-1"><span class="row-day-1-text">Day 1</span></div>
+      <div class="program-day-2">Day 2</div>
+    </div>
     <div class="program-table">
       <div class="program-row program-baseline">
-        <div class="row-day-1">Day 1</div>
-        <div class="row-day-2">Day 2</div>
-      </div>
-      <div class="program-row program-baseline">
-        <span class="row-time">08:00 - 10:00</span>
-        <span class="row row-yellow">Registration &amp; Coffee</span>
-      </div>
-      <div class="program-row program-baseline">
-        <span class="row-time">10:00 - 11:30</span>
-        <span class="row row-yellow">Opening Ceremony</span>
+        <span class="row-time">09:00</span>
+        <span class="row row-yellow">
+          <span>Registration</span>
+          <span class="row-small-text">(Open until 12:00)</span></span
+        >
       </div>
       <div class="program-row program-center">
-        <span class="row-time">11:30 - 12:00</span>
+        <span class="row-time"></span>
+        <div class="row-items">
+          <div class="row-item">
+            <span class="row-item-time"
+              >09:10 <span class="row-item-time-duration">(20 mins)</span></span
+            >
+            <span class="row-item-title">Judge Briefing</span>
+            <span class="row-item-venue venue-prep">Preparation Room</span>
+          </div>
+          <div class="row-item">
+            <span class="row-item-time"
+              >09:40 <span class="row-item-time-duration">(70 mins)</span></span
+            >
+            <span class="row-item-title"
+              >International Speech Run-off A Contest</span
+            >
+            <span class="row-item-venue venue-lisianthus">Room Lisianthus</span>
+          </div>
+          <div class="row-item">
+            <span class="row-item-time"
+              >09:40 <span class="row-item-time-duration">(70 mins)</span></span
+            >
+            <span class="row-item-title"
+              >International Speech Run-off B Contest</span
+            >
+            <span class="row-item-venue venue-clove">Room Clove</span>
+          </div>
+        </div>
+      </div>
+      <div class="program-row program-center">
+        <span class="row-time">11:00</span>
         <div class="row-items">
           <div class="row-item-expand">
-            <span class="row-item-speaker">John Hei</span>
-            <span class="row-item-title">On Becoming a Leader</span>
-            <span class="row-item-venue venue-2c">Room 2C</span>
+            <span class="row-item-time">Keynote Speech - Stan Shih</span>
+            <span class="row-item-title">TBD</span>
+            <span class="row-item-venue venue-calla"
+              >Room Calla Lilly & Rose</span
+            >
           </div>
         </div>
       </div>
       <div class="program-row program-baseline">
-        <span class="row-time">12:00 - 13:00</span>
-        <span class="row row-gray">Talks &amp; Bites</span>
+        <span class="row-time">12:00</span>
+        <span class="row row-gray">
+          <span>Lunch Time <span class="row-small-text">(30 mins)</span></span>
+          <span class="row-small-text">(Lunchbox Distribution)</span></span
+        >
       </div>
       <div class="program-row program-center">
-        <span class="row-time">13:00 - 14:00</span>
+        <span class="row-time"></span>
         <div class="row-items">
           <div class="row-item">
-            <span class="row-item-speaker">Bachir Bastie</span>
-            <span class="row-item-title">Better Life</span>
-            <span class="row-item-venue venue-b2">Room B2</span>
+            <span class="row-item-time"
+              >12:10 <span class="row-item-time-duration">(20 mins)</span></span
+            >
+            <span class="row-item-title">Judge Briefing</span>
+            <span class="row-item-venue venue-prep">Preparation Room</span>
           </div>
           <div class="row-item">
-            <span class="row-item-speaker">John Hei</span>
-            <span class="row-item-title">On Becoming a Leader</span>
-            <span class="row-item-venue venue-2c">Room 2C</span>
+            <span class="row-item-time"
+              >12:30 <span class="row-item-time-duration">(40 mins)</span></span
+            >
+            <span class="row-item-title">Opening Ceremony</span>
+            <span class="row-item-venue venue-calla"
+              >Room Calla Lilly & Rose</span
+            >
           </div>
-          <div class="row-item">
-            <span class="row-item-speaker">John Hei</span>
-            <span class="row-item-title">On Becoming a Leader</span>
-            <span class="row-item-venue venue-2d">Room 2D</span>
+        </div>
+      </div>
+      <div class="program-row program-center">
+        <span class="row-time">13:10</span>
+        <div class="row-items">
+          <div class="row-item-expand">
+            <span class="row-item-time">Keynote Speech - Greg Van Borssum</span>
+            <span class="row-item-title">TBD</span>
+            <span class="row-item-venue venue-calla"
+              >Room Calla Lilly & Rose</span
+            >
           </div>
         </div>
       </div>
       <div class="program-row program-baseline">
-        <span class="row-time">12:00 - 13:00</span>
-        <span class="row row-gray">Talks &amp; Bites</span>
+        <span class="row-time">14:00</span>
+        <span class="row row-gray gray-1-line">
+          <span>Tea Break <span class="row-small-text">(20 mins)</span></span>
+        </span>
       </div>
       <div class="program-row program-center">
-        <span class="row-time">14:00 - 15:00</span>
+        <span class="row-time"></span>
         <div class="row-items">
           <div class="row-item">
-            <span class="row-item-speaker">Verity Price</span>
-            <span class="row-item-title">How to write a winning speech</span>
-            <span class="row-item-venue venue-b2">Room B2</span>
+            <span class="row-item-time"
+              >14:20 <span class="row-item-time-duration">(60 mins)</span></span
+            >
+            <span class="row-item-title">Mandarin Evaluation Contest</span>
+            <span class="row-item-venue venue-calla"
+              >Room Calla Lilly & Rose</span
+            >
           </div>
           <div class="row-item">
-            <span class="row-item-speaker">John Hei</span>
-            <span class="row-item-title">On Becoming a Leader</span>
-            <span class="row-item-venue venue-2c">Room 2C</span>
+            <span class="row-item-time"
+              >14:20 <span class="row-item-time-duration">(60 mins)</span></span
+            >
+            <span class="row-item-title">English Training Session</span>
+            <span class="row-item-venue venue-lisianthus">Room Lisianthus</span>
           </div>
           <div class="row-item">
-            <span class="row-item-speaker">John Hei</span>
-            <span class="row-item-title">On Becoming a Leader</span>
-            <span class="row-item-venue venue-2d">Room 2D</span>
+            <span class="row-item-time"
+              >14:20
+              <span class="row-item-time-duration">(120 mins)</span></span
+            >
+            <span class="row-item-title title-3-lines"
+              >Japanese Speech & Evaluation Contest and Training Session</span
+            >
+            <span class="row-item-venue venue-clove">Room Clove</span>
+          </div>
+        </div>
+      </div>
+      <div class="program-row program-center">
+        <span class="row-time"></span>
+        <div class="row-items">
+          <div class="row-item">
+            <span class="row-item-time"
+              >15:30 <span class="row-item-time-duration">(60 mins)</span></span
+            >
+            <span class="row-item-title">Taiwanese Evaluation Contest</span>
+            <span class="row-item-venue venue-lisianthus">Room Lisianthus</span>
+          </div>
+          <div class="row-item">
+            <span class="row-item-time"
+              >15:30 <span class="row-item-time-duration">(60 mins)</span></span
+            >
+            <span class="row-item-title">English Training Session</span>
+            <span class="row-item-venue venue-calla"
+              >Room Calla Lilly & Rose</span
+            >
+          </div>
+        </div>
+      </div>
+      <div class="program-row program-center">
+        <span class="row-time"></span>
+        <div class="row-items">
+          <div class="row-item">
+            <span class="row-item-time"
+              >16:40 <span class="row-item-time-duration">(90 mins)</span></span
+            >
+            <span class="row-item-title">English Evaluation Contest</span>
+            <span class="row-item-venue venue-calla"
+              >Room Calla Lilly & Rose</span
+            >
+          </div>
+          <div class="row-item">
+            <span class="row-item-time"
+              >16:40 <span class="row-item-time-duration">(90 mins)</span></span
+            >
+            <span class="row-item-title">Mandarin Training Session</span>
+            <span class="row-item-venue venue-lisianthus">Room Lisianthus</span>
           </div>
         </div>
       </div>
       <div class="program-row program-baseline">
-        <span class="row-time">15:00 - 15:30</span>
-        <span class="row row-gray">Talks &amp; Bites</span>
-      </div>
-      <div class="program-row program-center">
-        <span class="row-time">15:30 - 16:30</span>
-        <div class="row-items">
-          <div class="row-item">
-            <span class="row-item-speaker">Bachir Bastie</span>
-            <span class="row-item-title">Better Life</span>
-            <span class="row-item-venue venue-b2">Room B2</span>
-          </div>
-          <div class="row-item">
-            <span class="row-item-speaker">John Hei</span>
-            <span class="row-item-title">On Becoming a Leader</span>
-            <span class="row-item-venue venue-2c">Room 2C</span>
-          </div>
-          <div class="row-item">
-            <span class="row-item-speaker">John Hei</span>
-            <span class="row-item-title">On Becoming a Leader</span>
-            <span class="row-item-venue venue-2d">Room 2D</span>
-          </div>
-        </div>
-      </div>
-      <div class="program-row program-center">
-        <span class="row-time">16:30 - 17:30</span>
-        <div class="row-items">
-          <div class="row-item">
-            <span class="row-item-speaker">Verity Price</span>
-            <span class="row-item-title">How to write a winning speech</span>
-            <span class="row-item-venue venue-b2">Room B2</span>
-          </div>
-          <div class="row-item">
-            <span class="row-item-speaker">John Hei</span>
-            <span class="row-item-title">On Becoming a Leader</span>
-            <span class="row-item-venue venue-2c">Room 2C</span>
-          </div>
-          <div class="row-item">
-            <span class="row-item-speaker">John Hei</span>
-            <span class="row-item-title">On Becoming a Leader</span>
-            <span class="row-item-venue venue-2d">Room 2D</span>
-          </div>
-        </div>
-      </div>
-      <div class="program-row program-baseline">
-        <span class="row-time">17:30 - 20:30</span>
-        <span class="row row-yellow">Dinner Party &amp; Closing Ceremony</span>
+        <span class="row-time">18:30</span>
+        <span class="row row-yellow">
+          <span>Gala Dinner</span>
+          <span class="row-small-text">Banquet Hall (Jasmine)</span></span
+        >
       </div>
     </div>
   </section>
@@ -142,18 +203,65 @@
     font-weight: 600;
     line-height: 49px;
     margin-top: 50px;
-    margin-bottom: 32px;
+    margin-bottom: 59px;
+  }
+  .program-day-switcher {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 0px;
+    gap: 31px;
+    height: 70px;
+    margin-bottom: 20px;
+    .program-day-1 {
+      width: 596px;
+      height: 70px;
+      color: black;
+      background: url(@/assets/icon/home/program-day-1.svg);
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      font-size: 24px;
+      font-weight: 400;
+      letter-spacing: 0.05em;
+      line-height: 60px;
+      border-radius: 20px;
+      box-sizing: border-box;
+      gap: 10px;
+      padding: 10px;
+      cursor: pointer;
+    }
+    .program-day-2 {
+      width: 596px;
+      height: 70px;
+      color: black;
+      background: linear-gradient(-90deg, #ddc8bf 0%, #a7c4d4 100%);
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      font-size: 24px;
+      font-weight: 400;
+      letter-spacing: 0.05em;
+      line-height: 60px;
+      border-radius: 20px;
+      box-sizing: border-box;
+      gap: 10px;
+      padding: 10px;
+      cursor: pointer;
+    }
   }
   .program-table {
-    width: 1347px;
-    height: 1755px;
+    max-width: 1231px;
+    width: 93.54%;
     background: rgba(255, 255, 255, 0.25);
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
     border-radius: 50px;
-    padding-top: 100px;
+    padding-top: 47px;
     .program-row {
       display: flex;
       flex-direction: row;
@@ -170,7 +278,7 @@
   }
 }
 .row-time {
-  width: 168px;
+  width: 73px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -181,66 +289,36 @@
   line-height: 60px;
   margin-right: 23px;
 }
+.time-filter {
+  color: rgba(255, 255, 255, 0.25);
+}
 .row {
   width: 1055px;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   font-size: 24px;
   font-weight: 400;
   letter-spacing: 0.05em;
-  line-height: 60px;
+  line-height: 25px;
   border-radius: 20px;
   gap: 10px;
   padding: 0px 10px;
-}
-.row-day-1 {
-  width: 610px;
-  height: 70px;
-  color: black;
-  background: rgba(255, 255, 255, 0.25);
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
-  font-weight: 400;
-  letter-spacing: 0.05em;
-  line-height: 60px;
-  border-radius: 20px;
-  box-sizing: border-box;
-  gap: 10px;
-  padding: 10px;
-  margin-right: 35px;
-  cursor: pointer;
-}
-.row-day-2 {
-  width: 610px;
-  height: 70px;
-  color: black;
-  background: linear-gradient(-90deg, #ddc8bf 0%, #a7c4d4 100%);
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
-  font-weight: 400;
-  letter-spacing: 0.05em;
-  line-height: 60px;
-  border-radius: 20px;
-  box-sizing: border-box;
-  gap: 10px;
-  padding: 10px;
-  cursor: pointer;
+  .row-small-text {
+    font-size: 16px;
+  }
 }
 .row-yellow {
-  height: 90px;
+  height: 93px;
   background: rgba(255, 204, 77, 0.25);
 }
 .row-gray {
-  height: 60px;
+  height: 93px;
   background: rgba(182, 194, 210, 0.5);
+}
+.gray-1-line {
+  height: 60px;
 }
 .row-items {
   width: 1058px;
@@ -252,7 +330,7 @@
     margin-right: 16px;
   }
   .row-item-expand {
-    width: 1055px;
+    width: 100%;
     height: 180px;
     background: rgba(255, 255, 255, 0.6);
     display: flex;
@@ -267,23 +345,27 @@
     padding: 5px 20px;
   }
   .row-item {
-    width: 341px;
     height: 180px;
     background: rgba(255, 255, 255, 0.6);
     display: flex;
+    flex: 1;
     flex-direction: column;
     align-items: flex-start;
     border-radius: 20px;
     padding: 5px 20px;
-    .row-item-speaker {
+    .row-item-time {
       color: black;
       font-size: 24px;
       font-weight: 400;
       letter-spacing: 0.05em;
-      line-height: 60px;
+      line-height: 30px;
+      .row-item-time-duration {
+        font-size: 16px;
+      }
     }
     .row-item-title {
-      height: 180px;
+      width: 298px;
+      height: 75px;
       color: black;
       display: flex;
       flex-direction: column;
@@ -292,10 +374,16 @@
       font-size: 24px;
       line-height: 30px;
       text-align: left;
+      margin-top: 3px;
+      margin-bottom: 8px;
+    }
+    .title-3-lines {
+      font-size: 20px;
+      line-height: 25px;
+      letter-spacing: 0.05em;
     }
   }
   .row-item-venue {
-    width: 100px;
     height: 30px;
     color: #004165;
     display: flex;
@@ -308,15 +396,20 @@
     line-height: 60px;
     border-radius: 20px;
     gap: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
   }
-  .venue-2c {
+  .venue-prep {
+    background: #dde1e1;
+  }
+  .venue-lisianthus {
     background: #ddc8bf;
   }
-  .venue-2d {
+  .venue-clove {
     background: rgba(188, 155, 221, 0.5);
   }
-  .venue-b2 {
-    background: #dde1e1;
+  .venue-calla {
+    background: #c1ccbe;
   }
 }
 </style>
