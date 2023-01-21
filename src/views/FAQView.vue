b957433116013b171c8788c03619a78376ffb8ed
Don <dontwn@gmail.com>
2023-01-21 11:17:31 +0800

    Update FAQView page

--- src/views/FAQView.vue
+++ src/views/FAQView.vue
@@ -2,19 +2,9 @@
   <article id="faq" class="faq-container">
     <Breadcrumb v-once />
     <header class="faq-page-title">Frequently asked questions</header>
-    <div class="faq-buttons">
-      <div class="faq-button white">Ticket</div>
-      <div class="faq-button purple">Members</div>
-      <div class="faq-button purple">Officers</div>
-    </div>
     <div class="faq-questions">
-      <div
-        v-for="(item, key) in items"
-        :key="key"
-        class="faq-question"
-        @click="toggle(key)"
-      >
-        <div class="faq-row">
+      <div v-for="(item, key) in items" :key="key" class="faq-question">
+        <div class="faq-row" @click="toggle(key)">
           <span>{{ item.question }}</span>
           <i :class="item.icon"></i>
         </div>
@@ -42,38 +32,6 @@
     margin-top: 80px;
     margin-bottom: 80px;
   }
-  .faq-buttons {
-    width: 996px;
-    color: black;
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: flex-start;
-    margin: 0 auto;
-    .faq-button {
-      width: 156px;
-      height: 46px;
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-      font-size: 20px;
-      font-weight: 400;
-      line-height: 24px;
-      text-align: center;
-      gap: 8px;
-      border-radius: 71px;
-      box-sizing: border-box;
-      padding: 8px 12px;
-      margin-right: 10px;
-      cursor: pointer;
-    }
-    .white {
-      background: rgba(255, 255, 255, 0.75);
-    }
-    .purple {
-      background: linear-gradient(180deg, #ddc8bf 0%, #a7c4d4 100%);
-    }
-  }
   .faq-questions {
     color: black;
     margin-top: 20px;
@@ -95,11 +53,12 @@
       margin-top: 15px !important;
       margin-bottom: 15px !important;
       margin: 0 auto;
-      cursor: pointer;
       .faq-row {
+        width: 990px;
         display: flex;
         flex-direction: row;
         align-items: center;
+        cursor: pointer;
         .faq-icon {
           position: absolute;
           right: 30px;
@@ -107,9 +66,11 @@
       }
       .faq-answer {
         width: 910px;
+        min-height: 80px;
         font-size: 18px;
         line-height: 22px;
         text-align: left;
+        padding-top: 5px;
       }
     }
   }
@@ -138,11 +99,8 @@ export default {
   methods: {
     toggle(key) {
       this.items[key].show = !this.items[key].show;
-      if (this.items[key].icon === "pi pi-angle-down faq-icon") {
-        this.items[key].icon = "pi pi-angle-up faq-icon";
-      } else {
-        this.items[key].icon = "pi pi-angle-down faq-icon";
-      }
+      const direction = this.items[key].show ? "up" : "down";
+      this.items[key].icon = `pi pi-angle-${direction} faq-icon`;
     },
   },
 };
