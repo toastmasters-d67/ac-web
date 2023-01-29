150e020acf5d1df917faf70d7d3bbbc3a25b8f18
Don <dontwn@gmail.com>
2023-01-29 10:31:57 +0800

    vue add i18n

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,32 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true,
+    },
+  },
 });
 
 module.exports = {
   publicPath: process.env.BASE_URL,
+
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true,
+    },
+  },
 };
