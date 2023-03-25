7a6aabd9c841a30266ed9e23560bb242d940a368
Yi-Hsuan0727 <a3154129@gmail.com>
2023-03-25 20:06:24 +0800

    main.js typo fixed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router/index copy";
+import router from "./router/index";
 import Button from "primevue/button";
 import Sidebar from "primevue/sidebar";
 import PrimeVue from "primevue/config";
